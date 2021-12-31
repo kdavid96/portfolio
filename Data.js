4631d19d4894511066b531ae7fc297e932c9ab26
Dávid Koppány <57870734+kdavid96@users.noreply.github.com>
2021-12-31 12:43:25 +0100

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,22 @@
+export const links = [
+    {
+        id:1,
+        text:'Projects',
+        url:'#projects'
+    },
+    {
+        id:2,
+        text:'Contact',
+        url:'#contact'
+    },
+    {
+        id: 3,
+        text: 'CV',
+        url: '#cv'
+    },
+    {
+        id: 4,
+        text: 'Blog',
+        url: 'blog'
+    }
+]
