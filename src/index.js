595a207dc90d18cae29089debd43635efc106cdb
unknown <kopppanydavid@gmail.com>
2023-10-29 14:57:36 +0100

    Email.js settings fix #4

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.render(
   <React.StrictMode>
+    <ToastContainer
+      position="bottom-center"
+      autoClose={500}
+      hideProgressBar
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="colored"
+    />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
